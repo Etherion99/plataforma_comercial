8ef9cc8b4d491bb4743c4938c7d449cc6cfe2ee0
Etherion99 <37667366+Etherion99@users.noreply.github.com>
2021-03-16 11:05:25 -0500

    home

---
+++ resources/views/home.blade.php
@@ -0,0 +1,15 @@
+@extends('layouts.main')
+
+@section('title', 'prueba')
+
+@section('styles')
+    @parent
+
+    <link rel="stylesheet" href="hola">
+@endsection
+
+@section('scripts')
+    @parent
+
+    <link rel="stylesheet" href="hola">
+@endsection
