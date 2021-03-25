4a9ba55ed57bb21bfc195d25175195f8111b7736
Etherion99 <37667366+Etherion99@users.noreply.github.com>
2021-03-25 17:15:59 -0500

    routes

--- routes/web.php
+++ routes/web.php
@@ -4,3 +4,4 @@ namespace App\Http\Controllers;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', [WebController::class, 'home'])->name('home');
+Route::get('/categoria/{id}', [WebController::class, 'viewCategory'])->name('viewCategory');
