03b91f1155320a30cd0129b4b1e835b311d316c4
Arley Velasco <arley2180984@correo.uis.edu.co>
2021-03-29 11:18:45 -0500

    adfdafsadf

--- public/js/access.js
+++ public/js/access.js
@@ -48,7 +48,7 @@ function validateNav() {
 function validatePage() {}
 
 $(document).ready(function () {
-  navigate(0);
+  navigate(1);
   navNext.click(function () {
     navigate(1);
   });
@@ -56,6 +56,17 @@ $(document).ready(function () {
     navigate(-1);
   });
   navFinish.click();
+  $('#exampleModal').on('show.bs.modal', function (event) {
+    var button = $(event.relatedTarget); // Button that triggered the modal
+
+    var recipient = button.data('whatever'); // Extract info from data-* attributes
+    // If necessary, you could initiate an AJAX request here (and then do the updating in a callback).
+    // Update the modal's content. We'll use jQuery here, but you could use a data binding library or other methods instead.
+
+    var modal = $(this);
+    modal.find('.modal-title').text('New message to ' + recipient);
+    modal.find('.modal-body input').val(recipient);
+  });
 });
 /******/ })()
 ;
